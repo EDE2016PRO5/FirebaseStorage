efd2937ffd1cfdc923a56359b1be1d5b8129c72c
Kim Nielsen <kimnielsen33333@gmail.com>
2018-11-20 23:31:02 +0100

    Download fixed...

--- app/src/main/java/dk/pme/kim/storageexample/MainActivity.kt
+++ app/src/main/java/dk/pme/kim/storageexample/MainActivity.kt
@@ -1,8 +1,11 @@
 package dk.pme.kim.storageexample
 
+import android.app.usage.ExternalStorageStats
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
@@ -11,33 +14,33 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
 	private val url = "gs://storageexample-916c1.appspot.com"
 
 	//	Values for uploading:
-	private val mobilePath = "/storage/emulated/0/Download/onlinedata.txt"
-	private val firebasePath = "Test/onlinedata.txt"
+	private val mobilePath_upload = "/storage/emulated/0/Download/onlinedata.txt"
+	private val firebasePath_upload = "Test/onlinedata.txt"
 
 	//	Values for downloading:
-	private val filename = "GitHub App.pdf"
-	private val prefix = "test_prefix"
-	private val suffix = ".pdf"
+	private val mobilePath_download = Environment.getExternalStorageDirectory().toString()+"/Download"
+	private val firebasePath_download = "Test/onlinedata.txt"
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
 		//	Remember to enable usage of external data for app:
-		val file = Uri.fromFile(File(mobilePath))
+		val file = Uri.fromFile(File(mobilePath_upload))
 
-		uploadFile(url, file, firebasePath)
-		downloadFile(url, filename, prefix, suffix)
+		uploadFile(url, firebasePath_download, file)
+		downloadFile(url, firebasePath_download, "Github", ".txt", mobilePath_download)
 	}
 
 	//	Upload file:
-	fun uploadFile(url : String, file : Uri, path : String){
+	fun uploadFile(url : String, path : String, file : Uri){
 		val storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(url)
 		val fileRef = storageRef.child(path)
 
@@ -59,24 +62,42 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	//	Download file:
-	fun downloadFile(url : String, filename : String, prefix : String, suffix : String){
+	fun downloadFile(url : String, path : String, prefix : String, suffix : String, outPath : String){
 		val storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(url)
-		val fileRef = storageRef.child(filename)
-		val localFile = File.createTempFile(prefix, "suffix")
+		val fileRef = storageRef.child(path)
 
 		pBar_download.visibility = View.VISIBLE
 		pBar_upload.progress = pBar_upload.min
 
-		fileRef.getFile(localFile)
+		val file = File.createTempFile(prefix, suffix, File(outPath))
+
+		fileRef.getFile(file)
 				.addOnSuccessListener {
 					pBar_download.progress = pBar_upload.max
 					//pBar_download.visibility = View.GONE
 					Toast.makeText(this, "Download succesful!", Toast.LENGTH_LONG).show()
-					Log.i("Path", fileRef.path)
-					Log.i("AbsolutePath", localFile.absolutePath)
 				}
 				.addOnFailureListener {
+					Log.i("Path", fileRef.path)
+					Log.i("AbsolutePath", file.path)
+					Log.i("Download_error_cause", it.cause.toString())
 					Toast.makeText(this, "Download unsuccesful!", Toast.LENGTH_LONG).show()
 				}
 	}
+
+	/*
+	//	Check permission
+	fun askPermission() : Boolean {
+		if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+			return true
+		}
+		else{
+			if(shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+				Toast.makeText(this, "Storage permission is needed...",
+						Toast.LENGTH_LONG).show()
+			}
+
+			requestPermissions(String)
+		}
+	}*/
 }
